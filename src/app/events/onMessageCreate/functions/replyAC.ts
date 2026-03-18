7e1a887741d06df2f444a8ceabd41a51d6a66538
Jose Miguel <jo0303se@hotmail.com>
2026-03-18 14:07:53 -0500

    feat: enhance replyAC function to include user and conversation context

--- src/app/events/onMessageCreate/functions/replyAC.ts
+++ src/app/events/onMessageCreate/functions/replyAC.ts
@@ -1,10 +1,20 @@
 import type { Message } from 'whatsapp-web.js'
 import { getInstruction, callAgentWithMCP } from '@modules/agent'
-import { readPrompt } from '@/utils'
+import { readPrompt, readChatName, readUserName } from '@/utils'
+import { registerConversation, getChatConversation } from '@modules/querys'
 
 export async function replyAC(message: Message) {
+  const chatName = await readChatName(message)
   const newMessage = readPrompt(message)
-  const instruction = getInstruction('agentAC', newMessage)
+  const userName = await readUserName(message)
+  const conversation = await getChatConversation(`ac_${chatName}`, userName, newMessage)
+  const instruction = getInstruction('agentAC', conversation)
   const aiResponse = await callAgentWithMCP(instruction)
+  await registerConversation({
+    chat: `ac_${chatName}`,
+    userName: userName,
+    user_message: newMessage,
+    bot_response: aiResponse,
+  })
   await message.reply(aiResponse)
 }
